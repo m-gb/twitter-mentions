e75cbb9a0cfc47dd915914aed61310eca06e7589
m-gb <mayu666@gmail.com>
2018-01-24 21:11:32 +0100

    change variable names

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Twitter Mentions
 
-This is a command line application which searches Twitter for the 5 most recent tweets that mention a given Twitter screen name.
+This is a command line application which searches Twitter for the 5 most recent tweets that mention a given Twitter username.
 
 There are two search methods in the application:
 
@@ -11,11 +11,11 @@ There are two search methods in the application:
 
 To run the application, execute the following in a terminal:
 ```
-$ ./search_tweets.rb <screen name> <method> 
+$ ./search_tweets.rb <username> <method> 
 ```
-Without arguments, the application will ask for a screen name and use the `use_oauth` method.
+Without arguments, the application will ask for a username and use the `use_oauth` method.
 
-If a screen name is provided, `twitter` can be specified as a secondary argument to use the `use_twitter` method.
+If a username is provided, `twitter` can be specified as a secondary argument to use the `use_twitter` method.
 
 Note:
 The application requires two files to be present in its directory, called `access_token` and `consumer_key`, which can be generated at https://apps.twitter.com/.
--- search_tweets.rb
+++ search_tweets.rb
@@ -4,21 +4,21 @@ require 'oauth'
 require 'json'
 require 'twitter'
 
-screenname = ARGV[0]
+username = ARGV[0]
 method = ARGV[1]
 
 @consumer_key, @consumer_secret = File.read('./consumer_key').split("\n")
 @access_token, @access_secret = File.read('./access_token').split("\n")
 
-def use_oauth(screenname)
+def use_oauth(username)
   # Returns a consumer object which is required to request an oauth access token.
   consumer = OAuth::Consumer.new( @consumer_key, @consumer_secret,
                                   { site: "https://api.twitter.com/",
                                     scheme: :header })
   # Returns an oauth access token which can be used to communicate with the Twitter API.
   oauth_token = OAuth::AccessToken.new(consumer, @access_token, @access_secret)
-  # Searches tweets mentioning the screenname.
-  response = oauth_token.get("https://api.twitter.com/1.1/search/tweets.json?q=%40#{screenname}", result_type: 'recent')
+  # Searches tweets mentioning the username.
+  response = oauth_token.get("https://api.twitter.com/1.1/search/tweets.json?q=%40#{username}", result_type: 'recent')
   parsed = JSON.parse(response.body)
   tweets = []
   parsed['statuses'].each do |status| 
@@ -27,23 +27,23 @@ def use_oauth(screenname)
   puts tweets.take(5)
 end
 
-def use_twitter(screenname)
+def use_twitter(username)
   client = Twitter::REST::Client.new do |config|
     config.consumer_key = @consumer_key
     config.consumer_secret = @consumer_secret
   end
-  client.search("@#{screenname}", result_type: 'recent').take(5).map do |tweet|
-    puts "#{tweet.user.screen_name}: #{tweet.text}"
+  client.search("@#{username}", result_type: 'recent').take(5).map do |status|
+    puts "#{status.user.screen_name}: #{status.text}"
   end
 end
 
-unless screenname
-  puts 'Please enter a Twitter screen name to search for:'
-  screenname = gets.chomp
+unless username
+  puts 'Please enter a Twitter username to search for:'
+  username = gets.chomp
 end
 
 if method == 'twitter'
-  use_twitter(screenname)
+  use_twitter(username)
 else
-  use_oauth(screenname)
+  use_oauth(username)
 end
